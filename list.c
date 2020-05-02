089799ff46e90b4ea319a87b3bc4d950b4a2e609
Sruthy P S <58027046+pssruthy@users.noreply.github.com>
2020-05-02 13:15:30 +0530

    Added feature to add a number in the end of the list

--- list.c
+++ list.c
@@ -8,3 +8,23 @@ List_ptr create_list(void){
   pList->count = 0;
   return pList;
 }
+
+Node_ptr create_Node(int value){
+  Node_ptr node = malloc(sizeof(Node));
+  node->value = value;
+  node->next = NULL;
+  return node;
+}
+
+Status add_to_end(List_ptr pList, int value){
+  Node_ptr new_node = create_Node(value);
+  if(pList->head == NULL){
+    pList->head = new_node;
+  }else
+  {
+    pList->last->next = new_node;
+  }
+  pList->last = new_node;
+  pList->count++;
+  return Success;
+}
--- numbers.c
+++ numbers.c
@@ -7,19 +7,47 @@ void print_menu(void){
   printf("\nMain Menu\n");
   printf("---------\n");
   
-  printf("(m) exit\n");
+  printf("(a) add a number to the end of the list\n");
+  printf("(m) exit\n\n");
+
+  printf("Please enter the alphabet of the operation you would like to perform : \n");
 }
+
 int main(void)
 {
   char user_option;
+  int number;
+  Status status;
   List_ptr pList = create_list();
-  print_menu();
-  scanf("%c", &user_option);
-  while (user_option != 'm')
+
+
+print_menu();
+    scanf(" %c", &user_option);
+
+  while (user_option !='m')
   {
+    switch (user_option)
+    {
+    case 'a':
+      printf("Enter a number to add at the end of the list\n");
+      scanf("%d", &number);
+      status = add_to_end(pList, number);
+      if (status)
+      {
+        printf("Number inserted successfully\n");
+      }
+      else
+      {
+        printf("Insertion failed\n");
+      }
+      
+      break;
+
+    default :
+      printf("\nPlease enter a valid option \n");
+    }
     print_menu();
-    scanf("%c", &user_option);
+    scanf(" %c", &user_option);
   }
-  
   return 0;
 }
